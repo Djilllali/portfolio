f8a8ec6099f2c9d4b40b0a24c400e03708abc72f
Djillali <zr.djillali@gmail.com>
2023-12-27 12:31:05 +0100

    Fixed intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,12 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Djillali.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">4 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React.js && Node.js </span>
+        <span className="font-bold">Greetings, I'm Djillali,</span> a seasoned
+        full-stack developer boasting <span className="font-bold">4 years</span>{" "}
+        of hands-on experience. My passion lies in crafting{" "}
+        <span className="italic">websites and applications</span>, with a
+        particular focus on leveraging the capabilities of
+        <span className="underline"> React.js and Node.js.</span>
       </motion.h1>
 
       <motion.div
